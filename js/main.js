72a00c97832a66393a4bf12205c4f766bffc6599
Melanie Burger <melanie.brgr@gmail.com>
2016-05-04 22:51:09 -0400

    Basic gender toggle

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 angular.module('myApp', [])
 .controller('myController', function myController($scope) {
-	$scope.messages = [];
+	$scope.gender = "male";
+	$scope.genderText = {
+		male: {
+			hisHer: 'his'
+		},
+		female: {
+			hisHer: 'her'
+		}
+	};
+	$scope.genderTextHisHer = $scope.genderText[$scope.gender]['hisHer'];
+	$scope.updateText = function() {
+		$scope.genderTextHisHer = $scope.genderText[$scope.gender]['hisHer'];
+		console.log($scope.genderText[$scope.gender]['hisHer']);
+	}
 });
